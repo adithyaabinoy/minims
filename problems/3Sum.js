ab159a064bedd3697320b6256bf675427e550711
adithyaabinoy <adithyaabinoy@gmail.com>
2023-09-26 16:54:36 +0530

    psuedo code for 3 sum

---
+++ problems/3Sum.js
@@ -0,0 +1,6 @@
+//Psuedo code
+/* 3 sum is a question where we have to find the 3 elements with the given sum .
+step 1 : We are going to run nested for loops and the loop will be starting from i =0; for the first one and for the second its going to start from j = i +1 and for the last loop it will be k = j + 1;
+step 2 : we will use conditions to match the sum with sum of 3 elements */
+
+
