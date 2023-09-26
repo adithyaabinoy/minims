5ce5df962fb6b8c0c0bfcab09ede50d6e20fd5d2
adithyaabinoy <adithyaabinoy@gmail.com>
2023-09-26 18:03:50 +0530

    fizzbuzz

--- problems/fizzbuzz.js
+++ problems/fizzbuzz.js
@@ -1,3 +1,21 @@
 /*create a fizzbuzz function whch will take number parameter and based on that number we will apply conditions like num % 3 == 0 return buzz
 and so on for other conditions 
 */
+
+let num = 10;
+function fizzbuzz() {
+if(num % 3 == 0) {
+return "buzz";
+}
+if(num % 5 == 0) {
+return "fizz"
+}
+if(num % 15 == 0) {
+return "fizzbuzz"
+}
+return num;
+}
+
+let result = fizzbuzz(num)
+console.log(result)
+
