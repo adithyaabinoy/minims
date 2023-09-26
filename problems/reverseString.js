d9005487ed4a130c088d30dc6ee6b6b39e8eb165
adithyaabinoy <adithyaabinoy@gmail.com>
2023-09-26 17:42:32 +0530

    reverse string

--- problems/reverseString.js
+++ problems/reverseString.js
@@ -2,3 +2,10 @@
 step 1 : we used reverse for loop over the string 
 > pushed all the reversed elements inside an empty string.
 */
+
+let reverseStr = "hello world";
+let result = "";
+for(let i=reverseStr.length-1; i>=0; i--) {
+result += reverseStr[i];
+}
+console.log(result);
