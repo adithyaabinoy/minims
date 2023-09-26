2045b6c7afc875a8686f5756266f0d57a717403e
adithyaabinoy <adithyaabinoy@gmail.com>
2023-09-26 17:31:01 +0530

    psuedo code for reverse number

---
+++ problems/reverseNumber.js
@@ -0,0 +1,12 @@
+/*We have two approach 
+step 1 : by using inbuilt functions, where we will change the number into string by using toString() function .
+> Next we will change the string into array by using split() function.
+> Then we will use reverse() function to reverse the array of elements .
+> After reversing array we are going to change into string using join() function .
+> Then we will change the string into number by using parseInt() function . 
+
+step 2 : 
+> After changing into string we are going to use reverse for loop.
+>Pushing every element into an empty string
+>then we will change the string into number using parseInt() function .
+*/
